804b7bfe783f35b82a2bae5688cb45c2fab7d9ed
xWass <spectatorgaming02@gmail.com>
2022-10-18 10:58:47 -0400

    added tess to cmds

--- slashcmds/kill.js
+++ slashcmds/kill.js
@@ -9,7 +9,7 @@ module.exports={
 
     async execute(interaction) {
         console.log(`${ chalk.greenBright('[EVENT ACKNOWLEDGED]') } interactionCreate with command bot`);
-        let validIds=["176152457284550656", "928624781731983380"];
+        let validIds=["176152457284550656", "928624781731983380", "451229130072260608"];
 
         if (!validIds.includes(interaction.member.id)) {
             return interaction.reply({
