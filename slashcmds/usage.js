5a27475589dd73a1e65f7220b925b59b05da0486
xWass <spectatorgaming02@gmail.com>
2022-10-19 14:55:32 -0400

    save

---
+++ slashcmds/usage.js
@@ -0,0 +1,20 @@
+const {
+    SlashCommandBuilder
+}=require('@discordjs/builders');
+
+module.exports={
+    data: new SlashCommandBuilder()
+        .setName('usage')
+        .setDescription('View bot usage.'),
+    async execute(interaction, client) {
+        const usage = require('../usage.json')
+        interaction.reply({
+            embeds: [{
+                title: "Command Usage.",
+                description: `Build Request: ${usage.build.count} \nOrchan Build Request: ${usage.orchan.count}`,
+                color: 'GREEN',
+            }],
+            ephemeral: true
+        });
+    }
+};
