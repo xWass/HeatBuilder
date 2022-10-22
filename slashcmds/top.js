30007abd72883b2e76efe4824088dd877b17c898
xWass <spectatorgaming02@gmail.com>
2022-10-22 18:04:25 -0400

    cringe formatting of course

--- slashcmds/top.js
+++ slashcmds/top.js
@@ -22,13 +22,13 @@ module.exports={
         const type=interaction.options.getString('type');
         let build;
         if (type==="Track") {
-            build="\`\`\`Porsche 911 Carrera RSR 2.8 '73 \nVolkswagen Beetle '63\n McLaren F1 '93\n Ferrari F40 '87\n Mazda MX-5 '96\n Nissan Skyline 2000 GT-R '71\n Pagani Huayra BC '17\n Nissan Silvia Spec-R Aero '02\n Nissan Fairlady 240 ZG '71\n Ford GT '17\`\`\`"
+            build="\`\`\`Porsche 911 Carrera RSR 2.8 '73 \nVolkswagen Beetle '63\nMcLaren F1 '93\nFerrari F40 '87\nMazda MX-5 '96\nNissan Skyline 2000 GT-R '71\nPagani Huayra BC '17\nNissan Silvia Spec-R Aero '02\nNissan Fairlady 240 ZG '71\nFord GT '17\`\`\`"
         } else if (type==="Off-Road") {
-            build="\`\`\`Porsche 911 Carrera RSR 2.8 '73\n Volkswagen Beetle '63\n Volvo 242DL '75\n Mazda MX-5 '96\n Nissan 180SX Type X '96\n Honda Civic Type-R '00\n Ford Mustang Foxbody '90\n Porsche Cayman GT4 '15\n Mercedes AMG GT '15\n Acura RSX-S '04\`\`\`"
+            build="\`\`\`Porsche 911 Carrera RSR 2.8 '73\nVolkswagen Beetle '63\nVolvo 242DL '75\nMazda MX-5 '96\nNissan 180SX Type X '96\nHonda Civic Type-R '00\nFord Mustang Foxbody '90\nPorsche Cayman GT4 '15\nMercedes AMG GT '15\nAcura RSX-S '04\`\`\`"
         } else if (type==="Drag") {
-            build="\`\`\`Volkswagen Beetle '63\n Ferrari LaFerrari '13 (1x15 NOS, Manual, 3rd Gear Launch)\n Volvo 242DL '75\n Ferrari F40 '87\n Pagani Huayra BC '17\n Porsche 911 Carrera RSR 2.8 '73\n Honda NSX Type-R '92\n Nissan 180SX Type X '96\n Mazda MX-5 '96\n Ford Mustang Foxbody '90\`\`\`"
+            build="\`\`\`Volkswagen Beetle '63\nFerrari LaFerrari '13 (1x15 NOS, Manual, 3rd Gear Launch)\nVolvo 242DL '75\nFerrari F40 '87\nPagani Huayra BC '17\nPorsche 911 Carrera RSR 2.8 '73\nHonda NSX Type-R '92\nNissan 180SX Type X '96\nMazda MX-5 '96\nFord Mustang Foxbody '90\`\`\`"
         } else if (type==="Drift") {
-            build="\`\`\`Mazda RX-7 Spirit R '02\n Nissan 350Z '08\n Buick Grand National '87\n Chevrolet Corvette Z06 '13\n Volvo 242DL '75\n Chevrolet Corvette Grand Sport K.S '17\n Nissan Silvia Spec-R Aero '02\n Mitsubishi Lancer Evolution IX '07\n Nissan Fairlady 240 ZG '71\n Nissan 370Z Heritage Edition '19\`\`\`"
+            build="\`\`\`Mazda RX-7 Spirit R '02\nNissan 350Z '08\nBuick Grand National '87\nChevrolet Corvette Z06 '13\nVolvo 242DL '75\nChevrolet Corvette Grand Sport K.S '17\nNissan Silvia Spec-R Aero '02\nMitsubishi Lancer Evolution IX '07\nNissan Fairlady 240 ZG '71\nNissan 370Z Heritage Edition '19\`\`\`"
         }
 
         await interaction.reply({
