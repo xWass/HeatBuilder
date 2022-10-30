6420b14e1be639786eb684c480252b38ad3309b7
xWass <spectatorgaming02@gmail.com>
2022-10-30 09:15:39 -0400

    donations

--- slashcmds/support.js
+++ slashcmds/support.js
@@ -4,14 +4,14 @@ const chalk=require('chalk');
 module.exports={
     data: new SlashCommandBuilder()
         .setName('support')
-        .setDescription('Need support? Join here! \nhttps://discord.gg/X58z9eQGek'),
+        .setDescription('Need help? Join here! \nhttps://discord.gg/X58z9eQGek'),
 
     async execute(interaction) {
         console.log(`${ chalk.greenBright('[EVENT ACKNOWLEDGED]') } interactionCreate with command restart`);
 
         await interaction.reply({
             embeds: [{
-                title: "Need support? Join here! \nhttps://discord.gg/X58z9eQGek"
+                description: "Need help? Join here! \nhttps://discord.gg/X58z9eQGek\n\nWant to support this project? Message <@928624781731983380> or donate here: https://cash.app/$hoesinmydms"
             }],
             ephemeral: true
         });
